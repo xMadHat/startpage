c72eaa41fbe17c3890c03f297e54a08c41ae675d
xMadHat <luca.bilke@gmail.com>
2016-04-17 12:48:21 -0400

    Upload

---
+++ script/var.js
@@ -0,0 +1,89 @@
+// All the variable for the app
+// Their name are explicit
+
+var username = "MadHat"
+var clientid = "be212a58528168962a39c64052c1d88e";
+var redirecturi = "http://localhost:8002/callback.html";
+var locations = [
+  "2363527",
+  "638806"
+];
+var images = [
+  "1412446496031.jpg",
+  "1417804954510.jpg",
+  "1422771896804.jpg",
+  "1424655132831.jpg",
+  "1425939966695.jpg",
+  "1429482830109.jpg",
+  "1429569823779.jpg",
+  "1437167260211.jpg",
+  "1437214448937.jpg",
+  "1437214448937.jpg",
+  "1444816416285.jpg",
+  "1444827390885.jpg",
+  "1444827771939.jpg",
+  "1445223016379.jpg",
+  "1445223050369.jpg",
+  "1445370353275.jpg",
+  "1445705808951.jpg",
+  "1445713184723.jpg",
+  "1448399280112.jpg",
+  "1450066383293.jpg",
+  "1450074394745.jpg"
+];
+var searchs = [
+  ["!g", "https://www.google.ch/#q="],
+  ["!y", "https://www.youtube.com/results?search_query="],
+  ["!w", "https://en.wikipedia.org/w/index.php?search="]
+];
+var favorites = [
+  [ "Work",
+    [
+      ["edline" , "https://.edline.net/Index.page", "el"],
+      ["google docs" , "https://docs.google.com/document/u/0/", "gdc"],
+      ["gmail" , "https://mail.google.com/mail/u/0/#inbox", "gm"],
+      ["google drive" , "https://drive.google.com/drive/u/0/my-drive", "gdr"]
+    ]
+  ],
+  [ "Social",
+    [
+      ["messenger" , "https://.messenger.com/", "fm"],
+      ["facebook" , "https://.facebook.com/" , "fb"],
+	  ["raid" , "https://raidforums.com/", "rf"],
+      ["bscotch" , "https://forums.bscotch.net/index.php", "bs"]
+    ]
+  ],
+  [ "Download",
+    [
+      ["thepiratebay", "http://thepiratebay.se", "tpb"],
+      ["T411", "http://.t411.in/", "t411"]
+    ]
+  ],
+  [ "Frequent",
+    [
+      ["youtube", "https://.youtube.com/", "yt"],
+      ["inventory", "https://steamcommunity.com/id/banksy42/invent", "inv"],
+      ["netflix", "https://.netflix.com/browse", "nf"],
+      ["slither", "https://slither.io/", "sl"],
+      ["net config", "http://192.168.9.1/cgi-bin/luci", "nc"]
+    ]
+  ],
+  [ "4chan",
+    [
+      ["/b/", "http://4chan.org/b/", "b"],
+      ["/wg/", "http://4chan.org/wg/", "wg"],
+      ["/g/", "http://4chan.org/g/", "g"]
+    ]
+  ],
+  [ "Others",
+    [
+      ["glitcher", "http://jkirchartz.com/Glitchy3bitdither/GlitchCruiser.html", "g3b,
+      ["ifunny", "http://ifunny.com", "hdl"]
+    ]
+  ]
+];
+var feeds = [
+  ["cs:go","http://blog.counter-strike.net/index.php/feed/"],
+  ["cs:go news", "http://feeds.thescoreesports.com/csgo.rss"],
+  ["Listen to this", "https://www.reddit.com/r/listentothis/.rss"]
+];
